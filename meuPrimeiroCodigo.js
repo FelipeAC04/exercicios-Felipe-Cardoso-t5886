61dd077a9c5c8284b61e2c73a5afae92aa987324
FelipeAC04 <felipeemailcurso123@gmail.com>
2024-09-05 16:46:03 -0300

    Add files via upload

---
+++ meuPrimeiroCodigo.js
@@ -0,0 +1,33 @@
+/*
+let meuNome = "Felipe"
+
+let sobrenome = "Cardoso"
+
+let idade = 16 
+
+let souEstudante = true
+
+console.log("meu nome eh", meuNome, "meu sobrenome eh", sobrenome, "eu tenho", idade, "anos", "e sou estudante", souEstudante) */
+
+
+/*const got = "Game of Thrones"
+const temporadasDeGot = 8
+
+console.log("a constante got eh do tipo", typeof got)
+console.log("e armazena", typeof temporadasDeGot)*/
+
+/*let novaVariavel;
+console.log(typeof novaVariavel)*/
+
+/*let ano = "2012"
+console.log(typeof ano)
+
+
+let anoConvertidoParaNumber = Number(ano)
+console.log(typeof anoConvertidoParaNumber, anoConvertidoParaNumber)*/
+
+/*const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)*/
