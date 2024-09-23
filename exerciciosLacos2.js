ccd756d096fd43f84010fd6d77778205b7cd58c1
FelipeAC04 <felipeemailcurso123@gmail.com>
2024-09-23 13:29:39 -0300

    Add files via upload

---
+++ exerciciosLacos2.js
@@ -0,0 +1,161 @@
+const ask = require('readline-sync')
+
+/*
+Os exercícios de hoje são divididos em interpretação e escrita de código. 
+Para os de interpretação, coloque as respostas em comentários de código. 
+Já, nos de escrita, lembre-se de imprimir no console os resultados.
+
+
+█████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+███████ ██   ██ ███████ ██████   ██████ ██  ██████ ██  ██████  ███████     ██████  ███████ 
+██       ██ ██  ██      ██   ██ ██      ██ ██      ██ ██    ██ ██          ██   ██ ██      
+█████     ███   █████   ██████  ██      ██ ██      ██ ██    ██ ███████     ██   ██ █████   
+██       ██ ██  ██      ██   ██ ██      ██ ██      ██ ██    ██      ██     ██   ██ ██      
+███████ ██   ██ ███████ ██   ██  ██████ ██  ██████ ██  ██████  ███████     ██████  ███████ 
+
+██ ███    ██ ████████ ███████ ██████  ██████  ██████  ███████ ████████  █████   ██████  █████   ██████  
+██ ████   ██    ██    ██      ██   ██ ██   ██ ██   ██ ██         ██    ██   ██ ██      ██   ██ ██    ██ 
+██ ██ ██  ██    ██    █████   ██████  ██████  ██████  █████      ██    ███████ ██      ███████ ██    ██ 
+██ ██  ██ ██    ██    ██      ██   ██ ██      ██   ██ ██         ██    ██   ██ ██      ██   ██ ██    ██ 
+██ ██   ████    ██    ███████ ██   ██ ██      ██   ██ ███████    ██    ██   ██  ██████ ██   ██  ██████  
+
+
+Tente responder os exercícios dessa seção sem executar o código. 
+Se ficar muito difícil, pode rodar no seu computador para analisar e pensar sobre o resultado.                                                                                                                                                                                                
+
+█████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+
+
+//1. O que o código abaixo está fazendo? Qual o resultado impresso no console?
+
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+
+
+//2. Leia o código abaixo:
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+
+    //a) O que vai ser impresso no console?
+
+    //b) Se eu quisesse acessar o **índice** de cada elemento dessa lista, o `for...of...` é suficiente? Se sim, o que poderia ser usado para fazer isso?
+
+
+
+//3.Qual seria o resultado impresso no console, se o usuário digitasse o número 4 ?
+    const quantidadeTotal = Number(ask.question("Digite a quantidade de linhas: "))
+    let quantidadeAtual = 0
+    while(quantidadeAtual < quantidadeTotal){
+        let linha = ""
+        for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+            linha += "*"
+        }
+        console.log(linha)
+        quantidadeAtual++
+    }
+
+*/
+/*
+█████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+███████ ██   ██ ███████ ██████   ██████ ██  ██████ ██  ██████  ███████     ██████  ███████
+██       ██ ██  ██      ██   ██ ██      ██ ██      ██ ██    ██ ██          ██   ██ ██
+█████     ███   █████   ██████  ██      ██ ██      ██ ██    ██ ███████     ██   ██ █████
+██       ██ ██  ██      ██   ██ ██      ██ ██      ██ ██    ██      ██     ██   ██ ██
+███████ ██   ██ ███████ ██   ██  ██████ ██  ██████ ██  ██████  ███████     ██████  ███████
+
+███████ ███████  ██████ ██████  ██ ████████  █████      ██████  ███████
+██      ██      ██      ██   ██ ██    ██    ██   ██     ██   ██ ██
+█████   ███████ ██      ██████  ██    ██    ███████     ██   ██ █████
+██           ██ ██      ██   ██ ██    ██    ██   ██     ██   ██ ██
+███████ ███████  ██████ ██   ██ ██    ██    ██   ██     ██████  ███████
+
+ ██████  ██████  ██████  ██  ██████   ██████
+██      ██    ██ ██   ██ ██ ██       ██    ██
+██      ██    ██ ██   ██ ██ ██   ███ ██    ██
+██      ██    ██ ██   ██ ██ ██    ██ ██    ██
+ ██████  ██████  ██████  ██  ██████   ██████
+
+█████████████████████████████████████████████████████████████████████████████████████████████████████████
+*/
+
+
+/*
+1. Pergunte ao usuário quantos bichinhos de estimação ele tem e guarde esse dado em uma variável. 
+    
+    a) Se a quantidade for 0, imprima no console "Que pena! Você pode adotar um pet!"
+    
+    b) Se a quantidade for maior que 0, solicite que o usuário digite os nomes deles, um por um, e guarde esses nomes em um array    
+    
+    c) Por fim, imprima o array com os nomes dos bichinhos no console
+*/
+
+
+/*
+2. Considere que você tenha acesso a um `array`  (chamado de 'array original') que é composto somente de números. Baseando-se nisso, 
+crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
+    
+    a) Escreva um programa que **imprime** cada um dos valores do array original.
+    
+    b) Escreva um programa que **imprime** cada um dos valores do array original divididos por 10
+    
+    c) Escreva um programa que **crie** um novo array contendo, somente, os números pares do array original e **imprima** esse novo array
+    
+    d) Escreva um programa que **crie** um novo array contendo strings, da seguinte forma: "O elemento do índex `i` é: `numero`". Depois, **imprima** este novo array.
+    
+    e) Escreva um programa que imprima no console o maior e o menor números contidos no array original
+*/
+
+
+let arrayOriginal = [10, 15, 20]
+
+/*
+console.log("numeros do array um por um:")
+
+function imprimeArray() {
+for (numeroUmPorUm of arrayOriginal) {
+    console.log(numeroUmPorUm)
+    }
+}
+
+imprimeArray()
+
+
+console.log("numeros do array divididos por 2")
+
+function imprimeArrayDivididoPor10(params) {
+    for (numeroUmPorUm of arrayOriginal) {
+        console.log(numeroUmPorUm / 10)
+        }
+}
+
+imprimeArrayDivididoPor10()
+*/
+
+console.log("Novo array somente com os numeros pares:")
+
+
+
+function retornaArrayComPares() {
+    let arrayPar = [];
+    arrayOriginal = [10, 15, 20];
+
+for (numeros of arrayOriginal) {
+    if((numeros % 2) == 0) {
+        arrayPar.push(numeros);
+        
+    }
+  }
+return arrayPar;
+}
+  
+console.log(retornaArrayComPares()) 
