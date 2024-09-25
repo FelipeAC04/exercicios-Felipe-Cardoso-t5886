d40466d3f488e468e8359d9d54477eafeb8ff3e5
FelipeAC04 <felipeemailcurso123@gmail.com>
2024-09-25 16:42:47 -0300

    Add files via upload

--- calculadoraFuncoes.js
+++ calculadoraFuncoes.js
@@ -1,13 +1,14 @@
 const ask = require("readline-sync")
 
-////////////////////////////VARIAVEIS-DE-SUPORTE///////////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////VARIAVEIS-AUXILIARES///////////////////////////////////////////////////////////////////////////////////////
 
 let resultadoFinal
-let fazerMaiscalculosOuExit = true
-let resposta
+let fazerMaiscalculosOuSair = true
 
 
-///////////////////////////////////OPERAÇÕES//////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////FUNÇÕES-PARA-AS-OPERAÇÕES///////////////////////////////////////////////////////////////////////////
 
 function realizaAOperacaoDeSoma(num1, num2) {
     return console.log(`Seus numeros são: ${num1} e ${num2}\nresultado da soma entre eles:`), resultadoFinal = num1 + num2
@@ -24,62 +25,82 @@ function realizaAOperacaoDeDivisao(num1, num2) {
 function realizaAOperacaoDeMOD(num1, num2) {
     return console.log(`Seus numeros são: ${num1} e ${num2}\nresultado do MOD entre eles:`), resultadoFinal = num1 % num2
 }
+function fazerMaiscalculosOuFecharPrograma() {
+    return fazerMaiscalculosOuSair = false
+}
 
 
-///////////////////////////////////CALCULADORA//////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////CÓDIGO-DA-CALCULADORA///////////////////////////////////////////////////////////////////////////////
 
 function calculadoraFuncionando() {
+
     console.clear()
 
-    while (fazerMaiscalculosOuExit) {
-        let num1 = Number(ask.question("Diga o seu primeiro numero: "))
-        let num2 = Number(ask.question("Diga o seu segundo numero: "))
+    while (fazerMaiscalculosOuSair) {
+        let num1 = Number(ask.question("Digite o seu primeiro numero --> "))
+        let num2 = Number(ask.question("Digite o seu segundo numero --> "))
         let tipoDeOperaçao = Number(ask.question(
             "______________________________________\n" +
-            "|Diga o tipo de operacao:            |\n" +
+            "|-----ESCOLHA O TIPO DE OPERACAO-----|\n" +
+            "|____________________________________|\n" +
             "|Digite 1 para adicao (+)            |\n" +
+            "|____________________________________|\n" +
             "|Digite 2 para subtracao (-)         |\n" +
+            "|____________________________________|\n" +
             "|Digite 3 para multiplicacao (x)     |\n" +
+            "|____________________________________|\n" +
             "|Digite 4 para divisao (/)           |\n" +
+            "|____________________________________|\n" +
             "|Digite 5 para MOD (%)               |\n" +
+            "|____________________________________|\n" +
             "|Digite 6 para reinserir os numeros  |\n" +
+            "|____________________________________|\n" +
+            "|Digite 0 para fechar o programa     |\n" +
             "|____________________________________|\n\n" +
             " ------------->  "))
+
         console.clear()
 
         switch (tipoDeOperaçao) {
             case 1:
                 console.log(realizaAOperacaoDeSoma(num1, num2))
+                ask.question("pressione enter para continuar:")
+                console.clear()
                 break;
             case 2:
                 console.log(realizaAOperacaoDeSubtracao(num1, num2))
+                ask.question("pressione enter para continuar:")
+                console.clear()
                 break;
             case 3:
                 console.log(realizaAOperacaoDeMultiplicacao(num1, num2))
+                ask.question("pressione enter para continuar:")
+                console.clear()
                 break;
             case 4:
                 console.log(realizaAOperacaoDeDivisao(num1, num2))
+                ask.question("pressione enter para continuar:")
+                console.clear()
                 break;
             case 5:
                 console.log(realizaAOperacaoDeMOD(num1, num2))
+                ask.question("pressione enter para continuar:")
+                console.clear()
                 break;
             case 6:
                 calculadoraFuncionando()
                 break;
+            case 0:
+                fazerMaiscalculosOuFecharPrograma()
             default:
+                console.log("tipo de operação invalido!")
+                calculadoraFuncionando()
                 break;
         }
-
-        resposta = Number(ask.question("Voce deseja fazer outro calculo? digite 1 para continuar ou 2 para parar: "))
-        fazerMaiscalculosOuExit = resposta === 1
-        console.clear()
-
 
     }
 
 }
-
-///////////////////////EXECUÇÃO DO CODIGO////////////////////////////////////////////////////////////////////////////////////////////////////
 
 calculadoraFuncionando()
 
